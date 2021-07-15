fcdca0a883d31fb170ec2c3589cd0c2e2e3d8b32
Timofey Ermakov <ts.ermakov@yandex.ru>
2021-07-16 00:39:12 +1000

    Resources & activity with movie details added

--- app/src/main/java/com/timermakov/homeworkmtsteta/MainActivity.kt
+++ app/src/main/java/com/timermakov/homeworkmtsteta/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.timermakov.homeworkmtsteta
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_details)
     }
 }
